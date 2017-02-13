df43dc6af0f64217081d1c8c47b44bc20e814322
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-02-13 15:10:19 +0000

    First test. added console runner for xUnit.

--- Src/StructureAnalysis/PatternChecker.cs
+++ Src/StructureAnalysis/PatternChecker.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StructureAnalysis
+﻿namespace StructureAnalysis
 {
     public class PatternChecker
     {
+        public bool IsValidPattern => true;
     }
 }
--- Src/StructureAnalysisTests/PatternCheckerTests.cs
+++ Src/StructureAnalysisTests/PatternCheckerTests.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StructureAnalysis;
+using Xunit;
 
 namespace StructureAnalysisTests
 {
     public class PatternCheckerTests
     {
+        [Fact]
+        public void NewPatternChecker_IsValidPattern_IsTrue()
+        {
+            var patternChecker = new PatternChecker();
+
+            Assert.Equal(true, patternChecker.IsValidPattern);
+        }
     }
 }
