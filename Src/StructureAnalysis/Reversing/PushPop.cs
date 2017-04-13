48caca9b3c7a08266b41c77da0ba63dd1e81bada
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-04-13 20:49:35 +0100

    Simple tests for PushPop

---
+++ Src/StructureAnalysis/Reversing/PushPop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StructureAnalysis.Reversing
+{
+    public class PushPop<T>
+    {
+        public static object Reverse(List<T> list)
+        {
+            return list;
+        }
+    }
+}
---
+++ Src/StructureAnalysisTests/Reversing/PushPopTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using StructureAnalysis.Reversing;
+using Xunit;
+
+namespace StructureAnalysisTests.Reversing
+{
+    public class PushPopTests
+    {
+        [Fact]
+        public void EmptyList_EmptyLitst()
+        {
+            var outputList = PushPop<int>.Reverse(new List<int>());
+
+            Assert.Equal(new List<int>(), outputList);
+        }
+
+        [Fact]
+        public void SingleElementList_SingleElementList()
+        {
+            var outputList = PushPop<int>.Reverse(new List<int> { 3 });
+
+            Assert.Equal(new List<int> { 3 }, outputList);
+        }
+    }
+}
