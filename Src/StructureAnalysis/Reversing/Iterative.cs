f9deb2a7cef6ab81d1fcefc4946108a4f4524d14
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-04-13 20:37:45 +0100

    First test for empty list using iteration.

---
+++ Src/StructureAnalysis/Reversing/Iterative.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StructureAnalysis.Reversing
+{
+    public class Iterative<T>
+    {
+        public static object Reverse(List<T> list)
+        {
+            return new List<T>();
+        }
+    }
+}
---
+++ Src/StructureAnalysisTests/Reversing/IterativeTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using StructureAnalysis.Reversing;
+using Xunit;
+
+namespace StructureAnalysisTests.Reversing
+{
+    public class IterativeTests
+    {
+        [Fact]
+        public void EmptyList_EmptyLitst()
+        {
+            var outputList = Iterative<int>.Reverse(new List<int>());
+
+            Assert.Equal(new List<int>(), outputList);
+        }
+    }
+}
