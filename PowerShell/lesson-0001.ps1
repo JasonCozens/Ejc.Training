0f816dcce8fc485a7c6c174bb597997dcc30655f
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-04-26 19:55:31 +0100

    Start of PowerShell lessons.

---
+++ PowerShell/lesson-0001.ps1
@@ -0,0 +1,30 @@
+﻿<#
+.DESCRIPTION
+    PowerShell by EJC - Lesson 1.
+.NOTES
+    This lesson is very short as it is demonstrating how to provide help for
+    a script.
+#>
+Set-Location $PSScriptRoot
+
+$ScriptName = $MyInvocation.MyCommand.Name
+
+Clear-Host
+
+$Command = "Get-Help .\$ScriptName"
+
+Write-Host Example 1 - Ask for help. -> $Command
+
+Invoke-Expression $Command
+
+Read-Host
+
+Clear-Host
+
+$Command_2 = "Get-Help -Full .\$ScriptName"
+
+Write-Host Example 2 - Ask for more help. -> $Command_2
+
+Invoke-Expression $($Command_2)
+
+Read-Host
