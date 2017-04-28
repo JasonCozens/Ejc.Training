ed6b262f8480e49d46431e7e7de79918a66ffc39
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-04-28 20:44:09 +0100

    Truth tables for And and Or.

--- PowerShell/Pester/Simple.Tests.ps1
+++ PowerShell/Pester/Simple.Tests.ps1
@@ -5,3 +5,33 @@
         }
     }
 }
+
+Describe "Evluate Truth Tables" {
+    Context "And" {
+        It "False And False = False" {
+            $false -and $false | Should Be $false
+        }
+        It "True And False = False" {
+            $true -and $false | Should Be $false
+        }
+        It "False And True = False" {
+            $false -and $true | Should Be $false
+        }
+        It "True And True = True" {
+            $true -and $true | Should Be $true
+        }
+    }
+    Context "Or" {
+        It "False Or False = False" {
+            $false -or $false | Should Be $false
+        }
+        It "True Or False = False" {
+            $true -or $false | Should Be $true
+        }
+        It "False Or True = False" {
+            $false -or $true | Should Be $true
+        }
+        It "True Or True = True" {
+            $true -or $true | Should Be $true
+        }
+    }}
