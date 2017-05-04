54617faef405ff121b2040934e040c8ab6db999a
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-05-04 20:36:54 +0100

    Building up SQL analysis.

--- SqlServer/analysis-001.sql
+++ SqlServer/analysis-001.sql
@@ -16,3 +16,33 @@ SELECT OBJECT_DEFINITION (OBJECT_ID(N'ChangeDriverPin'))
 SELECT name, OBJECT_DEFINITION (OBJECT_ID(name)) FROM sysobjects
 WHERE Type = 'P'
 
+/*
+    Get the server info.
+*/
+EXEC sp_server_info
+
+/*
+	List of databases with size.
+*/
+EXEC sp_databases
+
+/*
+    Details about a table
+*/
+EXEC sp_statistics Audit
+
+/*
+    List of tables in database.
+*/
+EXEC sp_tables
+
+/*
+    Not sure about this.
+*/
+EXEC sp_autostats Audit
+
+/*
+    Last time tables were modified.
+	Structure was changed.
+*/
+SELECT name, [modify_date] FROM sys.tables
