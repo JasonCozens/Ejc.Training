29c927a7298a8a02d51662cfc017df92c1db2308
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2017-08-03 08:41:29 +0100

    Simple Pester example.

---
+++ PowerShell/Pester/Tests/deployment.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$scriptLocation = Split-Path -Parent $MyInvocation.MyCommand.Source | Split-Path -Parent
+$systemUnderTest = (Split-Path -Leaf $MyInvocation.MyCommand.Source) -replace '\.Tests\.', '.'
+"$scriptLocation\$systemUnderTest"
+. "$scriptLocation\$systemUnderTest"
+
+Describe "File Version Name" {
+
+    Context "Version is 10.26.11" {
+
+    $result = Get-FileVersionName -Version "10.26.11"
+
+        It "Returns correct file version name" {
+            $result | Should Be "Trunk-2.0-Commit_10.26.11"
+        }
+    }
+}
